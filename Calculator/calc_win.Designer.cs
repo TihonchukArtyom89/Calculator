cce29751755269338b5ae9ca97b40e761e724c2d
Тихончук Артём <artyom_tihonchuk_work@protonmail.com>
2022-04-07 17:58:27 +0300

    add operators group

--- Calculator/calc_win.Designer.cs
+++ Calculator/calc_win.Designer.cs
@@ -32,6 +32,7 @@ namespace Calculator
             this.dev_note = new System.Windows.Forms.Label();
             this.title_app = new System.Windows.Forms.Label();
             this.digits = new System.Windows.Forms.GroupBox();
+            this.operators = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // dev_note
@@ -62,11 +63,21 @@ namespace Calculator
             this.digits.TabStop = false;
             this.digits.Text = "Цифры";
             // 
+            // operators
+            // 
+            this.operators.Location = new System.Drawing.Point(221, 96);
+            this.operators.Name = "operators";
+            this.operators.Size = new System.Drawing.Size(123, 185);
+            this.operators.TabIndex = 3;
+            this.operators.TabStop = false;
+            this.operators.Text = "Операторы";
+            // 
             // calc_win
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(356, 325);
+            this.Controls.Add(this.operators);
             this.Controls.Add(this.digits);
             this.Controls.Add(this.title_app);
             this.Controls.Add(this.dev_note);
@@ -82,6 +93,7 @@ namespace Calculator
         private System.Windows.Forms.Label dev_note;
         private System.Windows.Forms.Label title_app;
         private System.Windows.Forms.GroupBox digits;
+        private System.Windows.Forms.GroupBox operators;
     }
 }
 
