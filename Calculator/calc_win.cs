b26398d5bc538f145a0f265f41a4f848d82f023d
Тихончук Артём <artyom_tihonchuk_work@protonmail.com>
2022-04-07 18:22:09 +0300

    add design of  app

--- Calculator/calc_win.cs
+++ Calculator/calc_win.cs
@@ -16,5 +16,75 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dev_note_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void title_app_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void digits_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operators_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
